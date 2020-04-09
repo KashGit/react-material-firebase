9bb5e492c4e662dbd5f27670bf053e193bfc9b2c
kashgit <kashif.nisar@gmail.com>
2020-04-09 12:43:08 +0530

    initial setup

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography} from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Material UI</Typography>
+    </div>
+  );
+}
+
+export default App;
